b70eba33fab321859153e49ea7ed438375aabae5
Sergey <ug.tiser@gmail.com>
2011-10-06 20:02:33 +0300

    config assets welcome css4

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,6 @@ module Thrice
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.compile = true
 
 #    config.assets.precompile += ['welcome.css']
   end
